ce8fda898da6734573023cda4a837c631b29656d
EmreBkts <redevil_emre@hotmail.com>
2023-11-02 14:20:09 +0300

    product sayfası eklendi

---
+++ src/assets/js/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+
+    $(".btn_ , [data-btn-ripple]")
+        .on("mousedown touchstart", function () {
+            $(this).css('transform', 'scale(0.95)');
+        })
+        .on("mouseup touchend", function () {
+            $(this).css('transform', 'scale(1)');
+        })
+
+
+});
+
+
