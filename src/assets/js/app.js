73e67f99a46dd8d0c1b546e2c694ad064034c9a3
EmreBkts <redevil_emre@hotmail.com>
2023-11-10 20:13:30 +0300

    sayfalar mobil sayfalara göre ayarlandı.

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,6 +1,13 @@
 $(document).ready(function () {
 
 
+    $(".open-left-slide-menu").click(function () {
+        $(".slide-menu").toggleClass("slide-left");
+    });
+    $('.slide-close-button, .slider-overlay').click(function () {
+        $(".slide-menu").removeClass("slide-left");
+    });
+
     $(".btn_ , [data-btn-ripple]")
         .on("mousedown touchstart", function () {
             $(this).css('transform', 'scale(0.95)');
@@ -9,7 +16,7 @@ $(document).ready(function () {
             $(this).css('transform', 'scale(1)');
         })
 
-    $(".dropdown_ .dropdown_trigger").click(function (){
+    $(".dropdown_ .dropdown_trigger").click(function () {
         const dropEl = $(this).closest(".dropdown_")
         $(this).toggleClass("dt-active");
 
@@ -17,6 +24,35 @@ $(document).ready(function () {
 
     })
 
+    const swiper = new Swiper("[data-slider-products]", {
+        slidesPerView: "auto",
+        spaceBetween: 10,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+
+
+    new Swiper("[data-slider-stories]", {
+        slidesPerView: "auto",
+        spaceBetween: 0,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+
+
+
+    $(".bps_ca_total").click(function () {
+        $(".basket-page").toggleClass("bp_mobile_box");
+    })
+
+    $(".bp-overlay").click(function () {
+        $(".basket-page").removeClass("bp_mobile_box");
+    })
+
+
+
 });
-
-
